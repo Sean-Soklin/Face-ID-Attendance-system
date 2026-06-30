e24730654de2a217da491f00582d1c57b3c511dc
prithymonynichslay <monynich.prithy@gmail.com>
2026-06-30 22:28:24 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,12 @@
 # Main code for attendance project
+# libiary 
+
+
+# 1. processing student images in student image folder  (name)
+# task : write code to process student image in student images folder , getting each student's name from the file 
+
+
+
+
+# 2. camera handle and face recognition handle (Prithy Monynich)
+
