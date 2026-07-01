8d5d3ef61438f1999ecda021e4c069f2213e34f1
prithymonynichslay <monynich.prithy@gmail.com>
2026-07-01 12:42:46 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@
 
 
 # 1. processing student images in student image folder  (Nimol)
-# task : write code to process student image in student images folder , getting each student's name from the file 
+# task : write code to process student image in student images folder , getting each student's name from the file , load face encodig 
 
 
 
