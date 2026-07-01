3af9bc6890f5f9d555b87e6f2d885414e8ed39e9
prithymonynichslay <monynich.prithy@gmail.com>
2026-07-01 19:32:15 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,18 +1,26 @@
 # Main code for attendance project
 # libiary 
 
-
-# 1. processing student images in student image folder  (Nimol)
-# task : 
-# write code to automatically load, process, and extract facial features (encodings) from a subfolder in  student images folder to prepare a face recognition system. 
-# cleans up the image filenames to use them as formatted student names.
-
-
-
-# 2. camera handle and face recognition handle (Monity , Nynich)
-# task : Monity : write code to open windows camera , adjsut camera frame lighting , scanning student faces
+# Monity
+# Create a module that loads the pre-computed face recognition database from encodings.pickle.
+# Extract the saved face encodings and their corresponding names into separate variables. 
+# If the file cannot be found, display an error message and terminate the program. Finally, initialize an empty set to keep 
+# track of students whose attendance has already been recorded so duplicate entries are prevented.
+# write code in this block
 
 
 
-# 3 task : Nynich : write code to do face recognition comparing face from real live on camera with imges in folder , if face matches mark present
-#(loop inside camera code)
+
+
+
+
+
+
+
+
+
+
+
+
+# Monynich : Handling camera , camera frame and face recognition
+
