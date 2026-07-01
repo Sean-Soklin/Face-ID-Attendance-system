1e8e3b28e5c2023a272bf7582c8077b9b4f7ca8b
prithymonynichslay <monynich.prithy@gmail.com>
2026-07-01 11:37:40 +0700

    Update extract_video.py

--- extract_video.py
+++ extract_video.py
@@ -1,5 +1,5 @@
 # code for extracting student images from video
-# (name )  
+# (Neary)  
 # task : write code for extracting student images from student video in order to do face recognition project more accurately 
 # when scaning student faces
 
